6ad133dcb06b571a9f309e61a36ee4bd377458e5
Bornit <alghenny@hotmail.com>
2024-05-15 16:11:53 -0400

    Component Extend Style - Ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,5 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
+import '../styles/custom-styles.css';
 
 const product = {
   id: '1',
@@ -18,17 +19,38 @@ export const ShoppingPage = () => {
           flexWrap: 'wrap'
         }}>
 
-        <ProductCard product={ product } >
-            <ProductImage />
-            <ProductTitle title={ 'Hola Mundo' } />
-            <ProductButtons/>
+        <ProductCard
+            product={ product }
+            className="bg-dark text-white"
+        >
+            <ProductCard.Image      className="custom-image"    />
+            <ProductCard.Title      className="text-bold"       />
+            <ProductCard.Buttons    className="custom-button"   /> 
         </ProductCard>
 
-        <ProductCard product={ product } >
-            <ProductCard.Image />
-            <ProductCard.Title/>
-            <ProductCard.Buttons /> 
+        <ProductCard
+            product={ product }
+            className="bg-dark text-white"
+        >
+            <ProductImage       className="custom-image" style={{ boxShadow: '10px 10px 10px 10px rgba(0,0,0,0.2)' }} />
+            <ProductTitle       className="text-bold" />
+            <ProductButtons     className="custom-button" />
         </ProductCard>
+
+        <ProductCard
+            product={ product }
+            style= {{
+                backgroundColor: '#70D1F8'
+            }}
+        >
+            <ProductImage style={{ boxShadow: '10px 10px 10px 10px rgba(0,0,0,0.2)' }} />
+            <ProductTitle style={{ fontWeight: 'bolder' }} />
+            <ProductButtons style={{
+                display: 'flex',
+                justifyContent: 'end'  
+            }} />
+        </ProductCard>
+
         </div>
     </div>
   )
